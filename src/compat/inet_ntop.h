57df9529e9404e7a91587877b824d3e50d2d3f57
drummer <drummer>
2001-07-26 17:39:35 +0000

    inet_pton, inet_ntop

---
+++ src/compat/inet_ntop.h
@@ -0,0 +1,40 @@
+/*
+ * inet_ntop.h
+ *   prototypes for inet_ntop.c
+ *
+ * $Id: inet_ntop.h,v 1.1 2001/07/26 17:39:35 drummer Exp $
+ */
+/*
+ * Copyright (C) 2000, 2001 Eggheads Development Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef _EGG_COMPAT_INET_NTOP_H
+#define _EGG_COMPAT_INET_NTOP_H
+
+/* #include "config.h" */
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#ifndef HAVE_INET_NTOP
+/* Use our own implementation. */
+const char *egg_inet_ntop(int af, const void *src, char *dst, socklen_t size);
+#else
+#  define egg_inet_ntop	inet_ntop
+#endif
+
+#endif	/* !__EGG_COMPAT_INET_NTOP_H */
