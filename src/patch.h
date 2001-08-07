db0ef8b639059a9f3e9689d31edcbc83f426b96b
poptix <poptix>
2001-08-07 13:57:28 +0000

    PATCH: helppatch.diff

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.539 2001/08/07 13:49:41 poptix Exp $
+ * $Id: patch.h,v 1.540 2001/08/07 13:57:28 poptix Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("997192164");		/* current unixtime */
+patch("997192632");		/* current unixtime */
 /*
  *
  *
  */
-patch("nice_whom");
+patch("helppatch.diff");
 /*
  *
  *
