646703dd94a3c984ac3e8f2dbe476d8cc7f671ca
guppy <guppy>
2001-08-09 04:07:25 +0000

    oops, I guess I should faithfully use addpatch eh?

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.540 2001/08/07 13:57:28 poptix Exp $
+ * $Id: patch.h,v 1.541 2001/08/09 04:07:25 guppy Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,12 +41,12 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("997192632");		/* current unixtime */
+patch("997330034");		/* current unixtime */
 /*
  *
  *
  */
-patch("helppatch.diff");
+patch("cygwinsharedtcl1");
 /*
  *
  *
