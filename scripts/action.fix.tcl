c4491902b4df35b53bf99fba5eafd710722aa65e
stdarg <stdarg>
2001-10-21 06:04:43 +0000

    Updated action.fix.tcl to use new filt_string.

--- scripts/action.fix.tcl
+++ scripts/action.fix.tcl
@@ -3,16 +3,20 @@
 # Tothwolf  25May1999: cleanup
 # Tothwolf  04Oct1999: changed proc names slightly
 #
-# $Id: action.fix.tcl,v 1.3 1999/12/21 17:35:07 fabian Exp $
+# $Id: action.fix.tcl,v 1.4 2001/10/21 06:04:43 stdarg Exp $
 
 # fix for mirc dcc chat /me's
 bind filt - "\001ACTION *\001" filt:dcc_action
 proc filt:dcc_action {idx text} {
-  return ".me [string trim [join [lrange [split $text] 1 end]] \001]"
+  global filt_string
+  set filt_string ".me [string trim [join [lrange [split $text] 1 end]] \001]"
+  return 0
 }
 
 # fix for telnet session /me's
 bind filt - "/me *" filt:telnet_action
 proc filt:telnet_action {idx text} {
-  return ".me [join [lrange [split $text] 1 end]]"
+  global filt_string
+  set filt_string ".me [join [lrange [split $text] 1 end]]"
+  return 0
 }
