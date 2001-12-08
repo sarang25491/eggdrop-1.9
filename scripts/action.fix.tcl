04273f433be3937e3ae51f3bd6ed07bc805ddff2
ite <ite>
2001-12-08 16:58:34 +0000

    Handle irssi (and some others) 'correct' messages for DCC CTCP. Patch by Matthew Hallacy <poptix@techmonkeys.org>

--- scripts/action.fix.tcl
+++ scripts/action.fix.tcl
@@ -2,14 +2,22 @@
 #
 # Tothwolf  25May1999: cleanup
 # Tothwolf  04Oct1999: changed proc names slightly
+# poptix    07Dec2001: handle irssi (and some others) "correct" messages for DCC CTCP
 #
-# $Id: action.fix.tcl,v 1.4 2001/10/21 06:04:43 stdarg Exp $
+# $Id: action.fix.tcl,v 1.5 2001/12/08 16:58:34 ite Exp $
 
 # fix for mirc dcc chat /me's
 bind filt - "\001ACTION *\001" filt:dcc_action
+bind filt - "CTCP_MESSAGE \001ACTION *\001" filt:dcc_action2
 proc filt:dcc_action {idx text} {
   global filt_string
   set filt_string ".me [string trim [join [lrange [split $text] 1 end]] \001]"
+  return 0
+}
+
+proc filt:dcc_action2 {idx text} {
+  global filt_string
+  set filt_string ".me [string trim [join [lrange [split $text] 2 end]] \001]"
   return 0
 }
 
