49b1cf61c450ef06f6df84fc16cc4bbd786b5b09
guppy <guppy>
2001-12-28 18:37:13 +0000

    added my cmd_servers patch from 1.6

--- src/patch.h
+++ src/patch.h
@@ -10,7 +10,7 @@
  * statement, leave the rest of the file alone, this allows better
  * overlapping patches.
  *
- * $Id: patch.h,v 1.563 2001/12/28 18:31:39 guppy Exp $
+ * $Id: patch.h,v 1.564 2001/12/28 18:37:14 guppy Exp $
  */
 /*
  * Copyright (C) 1997 Robey Pointer
@@ -41,17 +41,17 @@ patch("CVS");			/* CVS version */
  *
  *
  */
-patch("1009564068");		/* current unixtime */
+patch("1009564114");		/* current unixtime */
 /*
  *
  *
  */
-patch("localmodules");
+patch("cmd_servers");
 /*
  *
  *
  */
-patch("localmodules");
+patch("cmd_servers");
 /*
  *
  *
