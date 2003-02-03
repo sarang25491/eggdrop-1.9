424cadc86ab22ce65e3a706a6fee7ab9b57ee890
stdarg <stdarg>
2003-02-03 01:57:58 +0000

    *** empty log message ***

---
+++ modules/script/scriptlog.c
@@ -0,0 +1,17 @@
+#include <eggdrop/eggdrop.h>
+
+static int script_putlog(char *text)
+{
+	return putlog("m", "*", "%s", text);
+}
+
+static int script_putloglev(char *level, char *chan, char *text)
+{
+	return putlog(level, chan, "%s", text);
+}
+
+script_command_t script_log_cmds[] = {
+	{"", "putlog", script_putlog, NULL, 1, "s", "text", SCRIPT_INTEGER, 0},
+	{"", "putloglev", script_putloglev, NULL, 3, "sss", "level channel text", SCRIPT_INTEGER, 0},
+	{0}
+};
